3d01a2933015a3735a3f2d4d827503d6e76d9e40
cvasquezl <84115739+cvasquezl@users.noreply.github.com>
2021-10-27 11:14:26 -0300

    Primeros cambios del main

---
+++ build/precache-manifest.b578c17f04b5f236202a983e09243fb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c8074f683b7ae48c025a0d97089ce3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b78ec01399dcc069367b",
+    "url": "/static/css/2.ae047e5f.chunk.css"
+  },
+  {
+    "revision": "b78ec01399dcc069367b",
+    "url": "/static/js/2.20c53f5c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.20c53f5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "297ec916506bdd0a0f79",
+    "url": "/static/js/main.4962547f.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
